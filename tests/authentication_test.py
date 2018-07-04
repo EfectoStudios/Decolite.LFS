65e9b1d09a88fb48e2f5cb7506669699c29c88dc
Alejandro Espinosa <ra.becerra388@uniandes.edu.co>
2018-07-04 17:08:31 -0500

    Authentication test base skeleton added.

--- tests/authentication_test.py
+++ tests/authentication_test.py
@@ -1,1 +1,11 @@
 """Unit tests for authentication."""
+import unittest
+import src.service.authentication
+
+
+class AuthenticationTest(unittest.TestCase):
+    """Test case for git authentication via https."""
+
+    def test_credentials(self):
+        """Verify that credentials are being checked against a server."""
+        self.fail('test not implemented')
