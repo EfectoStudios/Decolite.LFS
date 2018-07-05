e5da26e168c29c4c1f0d02f0948b6e486721a6c5
Alejandro Espinosa <alejandro.e@efectostudios.com>
2018-07-05 16:53:12 -0500

    Added authentication

--- src/service/authentication.py
+++ src/service/authentication.py
@@ -1,1 +1,9 @@
 """Git authentication module."""
+import os
+
+
+def authenticate(username, password):
+    """Authenticate the user against the service."""
+    if username not in os.environ:
+        return False
+    return os.environ[username] == password
--- tests/authentication_test.py
+++ tests/authentication_test.py
@@ -1,11 +1,15 @@
 """Unit tests for authentication."""
+import mock
+import os
 import unittest
-import src.service.authentication
+from src.service.authentication import authenticate
 
 
 class AuthenticationTest(unittest.TestCase):
     """Test case for git authentication via https."""
 
+    @mock.patch.dict(os.environ, {'USERNAME': 'PASSWORD'})
     def test_credentials(self):
-        """Verify that credentials are being checked against a server."""
-        self.fail('test not implemented')
+        """Test the service authenticates against environment variables."""
+        self.assertTrue(authenticate('USERNAME', os.environ['USERNAME']))
+        self.assertFalse(authenticate('sutano', 'unaClaveMuySegura'))
