161caf417168bf340ffe52a5d8d481357161bb45
Alejandro Espinosa <alejandro.e@efectostudios.com>
2018-07-09 16:40:15 -0500

    skeleton for path parsing

--- src/handler/routes.py
+++ src/handler/routes.py
@@ -1,4 +1,15 @@
 """Module with routing utilities."""
 
+LOCKS = 'locks'
+BATCH = 'objects/batch'
 
-PATH_REGEX = r"/?(?P<owner>\\w+)/(?<repo>\\w+)/info/lfs/(<tail>()|())"
+REQUEST_TYPES = {'BATCH': LOCKS,
+                 'LOCKS': BATCH,
+                 'BASE': ''}
+
+PATH_REGEX = r"/?(?P<owner>\\w+)/(?<repo>\\w+.git)/info/lfs/(?<tail>\\w*)"
+
+
+def get_path_request(path):
+    """Return type of request determined by the path."""
+    pass
---
+++ tests/routes_test.py
@@ -0,0 +1,9 @@
+"""Unit tests for routing."""
+import unittest
+
+
+class RoutesTest(unittest.TestCase):
+    """Unit tests for routes."""
+
+    def test_path_rrequest():
+        """Test request parsing."""
